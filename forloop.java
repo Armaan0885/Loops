c22a768b39a121b844baa194d827b28b5ea6f906
Armaan0885 <armaansingh6424@gmail.com>
2024-11-20 15:27:41 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,18 @@
+/*public class forloop {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i = i + 1){
+            System.out.println("Hello world");
+        }
+    }
+}*/
+
+
+public class forloop {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i = i + 1){
+            System.out.print(i+" ");
+        }
+    }
+}
